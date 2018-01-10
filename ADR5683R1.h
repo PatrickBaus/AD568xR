77226ecbcce99eaf1b2addd67fef797893d7c945
Tobias Liebmann <tobias.liebmann@gmx.de>
2018-01-10 11:27:59 +0100

    Added the GPL to both files.

---
+++ ADR5683R1.h
@@ -0,0 +1,19 @@
+/*    
+ *  The ADR5683R1 library is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as 
+ *  published by the Free Software Foundation, either version 3 of the 
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef ADR5683R1
+#define ADR5683R1
+
+
+
+#endif
